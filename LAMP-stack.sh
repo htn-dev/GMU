fecd7b252f3d88dd12a85c5d5f53be6147be780c
HTN <96134288+htn-dev@users.noreply.github.com>
2022-08-08 11:43:15 -0400

    Update LAMP-stack.sh

--- LAMP-stack.sh
+++ LAMP-stack.sh
@@ -8,7 +8,7 @@ python --version
  
 # Install pip, a simple tool that will allow you to better use python tools and packages later on
 sudo apt install python3-pip
-
+ 
 # MySQL is a very popular database.  MariaDB is the open source fork from it
 sudo apt install mysql-server
 sudo mysql_secure_installation
