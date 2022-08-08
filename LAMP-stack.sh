e12d06e03bf9bd47c43cfc7b6d57d2ae588388c2
HTN <96134288+htn-dev@users.noreply.github.com>
2022-08-08 11:43:19 -0400

    Update LAMP-stack.sh

--- LAMP-stack.sh
+++ LAMP-stack.sh
@@ -5,7 +5,7 @@ sudo apt upgrade
 # create a symbolic link to python 3
 sudo ln -s /usr/bin/python3  /usr/bin/python
 python --version
- 
+  
 # Install pip, a simple tool that will allow you to better use python tools and packages later on
 sudo apt install python3-pip
  
